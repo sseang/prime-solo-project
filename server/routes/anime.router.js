0e7638737fe2ee50323d8d9ccc4b7eb08987711c
sseang <142060734+sseang@users.noreply.github.com>
2024-03-17 09:36:02 -0500

    added res.send to STRETCH route

--- server/routes/anime.router.js
+++ server/routes/anime.router.js
@@ -12,35 +12,71 @@ router.get('/', (req, res) => {
 
 const BASE_URL_ANIME = 'anilist.co/api/v2/oauth/authorize';
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  const { searchTerm } = req.body;
-  // What to do???
-  axios
-    .get(
-      'https://anilist.co/api/v2/oauth/authorize?client_id=' +
-        clientData.clientId +
-        '&redirect_uri=' +
-        clientData.redirectUri +
-        '&response_type=code'
-    )
-    .then((aniListResponse) => {
-      console.log('SUCCESS', aniListResponse.data);
-      const simpleResults = aniListResponse.data.data;
-      //   return {
-      //     id: animeData.id,
-      //     url: animeData.images.original.url,
-      //     alt: animeData.title,
-      //   };
-      // });
-      res.send(simpleResults);
-    })
-    .catch((error) => {
-      console.log('ERROR:', error);
-      res.sendStatus(500);
-    });
-});
+//POST route template
 
+router.post('/', (req, res) => {});
+
+//TODO: STRETCH Goal incorporate API in to solo
+// router.post('/', (req, res) => {
+//   var query = `
+
+//   query ($id: Int, $page: Int, $perPage: Int) {
+//     Page (page: $page, perPage: $perPage) {
+//       pageInfo {
+//          total
+//           perPage
+//           currentPage
+//           lastPage
+//           hasNextPage
+//       }
+//     media (id: $id,  type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+//      id
+//       title {
+//         romaji
+//         english}
+//       }
+//     }
+//   }
+
+//   `;
+
+//   // Define our query variables and values that will be used in the query request
+//   var variables = {
+//     id: 15125,
+//   };
+
+//   // Define the config we'll need for our Api request
+//   var url = 'https://graphql.anilist.co',
+//     options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//       },
+//       body: JSON.stringify({
+//         query: query,
+//         variables: variables,
+//       }),
+//     };
+
+//   // Make the HTTP Api request
+//   fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
+
+//   function handleResponse(response) {
+//     return response.json().then(function (json) {
+//       return response.ok ? json : Promise.reject(json);
+//     });
+//   }
+
+//   function handleData(data) {
+//     console.log(data);
+//     res.send(data);
+//   }
+
+//   function handleError(error) {
+//     alert('Error, check console');
+//     console.error(error);
+//     res.sendStatus(500);
+//   }
+// });
 module.exports = router;
