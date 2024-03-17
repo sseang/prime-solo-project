6d681c625afce2590ed0a763415518073426fc7e
sseang <142060734+sseang@users.noreply.github.com>
2024-03-17 08:18:18 -0500

    created clientData for API call

--- server/routes/anime.router.js
+++ server/routes/anime.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
+const clientData = require('../modules/clientData');
+const axios = require('axios');
 /**
  * GET route template
  */
@@ -9,64 +10,37 @@ router.get('/', (req, res) => {
   // GET route code here
 });
 
+const BASE_URL_ANIME = 'anilist.co/api/v2/oauth/authorize';
+
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
-  // Here we define our query as a multi-line string
-  // Storing it in a separate .graphql/.gql file is also possible
-  //{ # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-  //{ # Define which variables will be used in the query (id)
-  var query = `
-  query ($id: Int) 
-    Media (id: $id, type: ANIME) 
-    id
-    title {
-      romaji
-      english
-      native
-    }
-  }
-}
-`;
-
-  // Define our query variables and values that will be used in the query request
-  var variables = {
-    id: 15125,
-  };
-
-  // Define the config we'll need for our Api request
-  var url = 'https://graphql.anilist.co',
-    options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        query: query,
-        variables: variables,
-      }),
-    };
-
-  // Make the HTTP Api request
-  fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
-
-  function handleResponse(response) {
-    return response.json().then(function (json) {
-      return response.ok ? json : Promise.reject(json);
+  const { searchTerm } = req.body;
+  // What to do???
+  axios
+    .get(
+      'https://anilist.co/api/v2/oauth/authorize?client_id=' +
+        clientData.clientId +
+        '&redirect_uri=' +
+        clientData.redirectUri +
+        '&response_type=code'
+    )
+    .then((aniListResponse) => {
+      console.log('SUCCESS', aniListResponse.data);
+      const simpleResults = aniListResponse.data.data;
+      //   return {
+      //     id: animeData.id,
+      //     url: animeData.images.original.url,
+      //     alt: animeData.title,
+      //   };
+      // });
+      res.send(simpleResults);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      res.sendStatus(500);
     });
-  }
-
-  function handleData(data) {
-    console.log(data);
-  }
-
-  function handleError(error) {
-    alert('Error, check console');
-    console.error(error);
-  }
 });
 
 module.exports = router;
